af38acfddf1ef4823e328114d06672e921c4e451
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-04-04 11:10:53 -0600

    Add clearCart and removeItem reducers

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,8 +1,11 @@
+import { clearCart } from '../features/cart/cartSlice';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const CartContainer = () => {
+  const dispatch = useDispatch();
   const { cartItems, total, amount } = useSelector((store) => store.cart);
+  
   if (amount < 1) {
     return (
       <section className='cart'>
@@ -31,7 +34,7 @@ const CartContainer = () => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button className='btn clear-btn'>clear cart</button>
+        <button className='btn clear-btn' onClick={() => dispatch(clearCart())} >clear cart</button>
       </footer>
     </section>
   );
